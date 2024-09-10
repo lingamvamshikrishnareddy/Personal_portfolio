7d5d09ed4456ebb61b326634fdcbbc57d793e6a8
Lingam Vamshi Krishna Reddy <125525582+lingamvamshikrishnareddy@users.noreply.github.com>
2024-09-10 07:08:47 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const apiRoutes = require('./routes/api'); // Adjust the path if necessary
+const apiRoutes = require('./routes/api');
 const { connectDB } = require('./config/database');
-const path = require('path');
+
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
 connectDB();
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'frontend/build')));
-
-// API routes
 app.use('/api', apiRoutes);
-
-// Serve the frontend application
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-});
 
 module.exports = app;
