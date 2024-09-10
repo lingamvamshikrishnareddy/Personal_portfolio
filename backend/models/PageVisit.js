821cdb5ce320012cd2f7990e536143ccff597f58
Lingam Vamshi Krishna Reddy <125525582+lingamvamshikrishnareddy@users.noreply.github.com>
2024-09-10 21:47:16 +0530

    Update PageVisit.js

--- backend/models/PageVisit.js
+++ backend/models/PageVisit.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
 
-const PageVisitSchema = new mongoose.Schema({
+const pageVisitSchema = new mongoose.Schema({
   page: {
     type: String,
     required: true,
+    unique: true
   },
   visitCount: {
     type: Number,
-    default: 1,
+    default: 1
   },
   lastVisited: {
     type: Date,
-    default: Date.now,
-  },
+    default: Date.now
+  }
 });
 
-module.exports = mongoose.model('PageVisit', PageVisitSchema);
+module.exports = mongoose.model('PageVisit', pageVisitSchema);
